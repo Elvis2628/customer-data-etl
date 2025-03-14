ee7250d12b3ec8cdb36bd0626fb26050e6e8099f
Elvis Enyiaka <elvix2626800@gmail.com>
2025-03-14 03:50:39 +0000

    Standardized Customer Names Format

--- transform/clean_customers.sql
+++ transform/clean_customers.sql
@@ -11,10 +11,18 @@ set country = "unknown"
 where country is null;
 
 
--- 2. Fix format inconsistencies
+-- 2. Fix format inconsistencies by ensuring: 
+-- a. all emails are in lowercase
 update sample_dataset
 set email = lower(email)
 where email is not null;
+
+
+-- b.  all customer names have first letter capitalized
+update sample_dataset
+set name = initcap(name)
+where name is not null;
+
 
 -- 3. Remove duplicate customer email
 delete from sample_dataset
